fdbf248f3045801d505fa30669b56befe9ebda13
Hagar Elbakry <hagarelbakry12@gmail.com>
2025-03-12 23:09:55 +0200

    add wishlist model and migration

---
+++ database/migrations/2025_03_12_210700_create_wish_lists_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Models\Account;
+use App\Models\Category;
+use App\Models\Product;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('wish_lists', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Account::class)->constrained()
+                ->onDelete('cascade');
+            $table->foreignIdFor(Product::class)->constrained()
+                ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('wish_lists');
+    }
+};
