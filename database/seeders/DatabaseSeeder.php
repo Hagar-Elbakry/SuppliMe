e8ff7e87318fe042f84f413ac47308638851140b
Menna-Baligh <mennabaligh55520@yahoo.com>
2025-04-14 21:20:17 +0200

    add discount seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,7 +23,8 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             CategorySeeder::class,
             ProductSeeder::class,
-            SupportMessageSeeder::class
+            SupportMessageSeeder::class,
+            DiscountSeeder::class
         ]);
     }
 }
---
+++ database/seeders/DiscountSeeder.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Discount;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DiscountSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Discount::create([
+            'discount_percentage' => 15.00 ,
+            'discount_type' => 'product',
+            'is_daily' => false,
+            'product_id' => 1,
+            'category_id' => null,
+            'start_date' => now(),
+            'end_date' => now()->addDays(7)
+        ]);
+        Discount::create([
+            'discount_percentage' => 10.00 ,
+            'discount_type' => 'product',
+            'is_daily' => true,
+            'product_id' => 11,
+            'category_id' => null,
+            'start_date' => now(),
+            'end_date' => now()->addDays(1)
+        ]);
+        Discount::create([
+            'discount_percentage' => 20.00,
+                'discount_type' => 'category',
+                'is_daily' => false,
+                'product_id' => null,
+                'category_id' => 3,
+                'start_date' => now(),
+                'end_date' => now()->addDays(30),
+        ]);
+    }
+}
