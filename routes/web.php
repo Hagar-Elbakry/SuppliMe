436aed70f1da3a716da99cbb6dff3380afc41425
Menna-Baligh <mennabaligh55520@yahoo.com>
2025-04-19 17:30:24 +0200

    updated Routes

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,19 @@
 use App\Http\Controllers\AuthenticatedSessionController;
 use App\Http\Controllers\CartController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\NotificationController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\RegisteredUserController;
+use App\Models\Notification;
 use Illuminate\Support\Facades\Route;
 
+    Route::get('/cart',[CartController::class,'index'])->name('cart.index');
+    Route::post('/cart/{product}',[CartController::class,'store'])->name('cart.store');
+    Route::get('/cart/{product_id}/update/{action}', [CartController::class, 'updateQuantity'])->name('cart.updateQuantity');
+    Route::delete('/cart',[CartController::class,'destroyAll'])->name('cart.destroyAll');
+    Route::delete('/cart/{product}',[CartController::class,'destroy'])->name('cart.destroy');
+    Route::get('/notifications',[NotificationController::class,'index'])->name('notifications.index');
+    Route::get('/',[HomeController::class,'index'])->name('home');
 
 
 Route::middleware('auth')->group(function () {
@@ -15,12 +24,6 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile/{user:name}/update',[ProfileController::class,'update'])->name('profile.update');
     Route::delete('/profile/{user:name}/destroy',[ProfileController::class,'destroy'])->name('profile.destroy');
     Route::post('/logout',[AuthenticatedSessionController::class,'destroy'])->name('logout');
-    Route::get('/cart',[CartController::class,'index'])->name('cart.index');
-    Route::post('/cart/{product}',[CartController::class,'store'])->name('cart.store');
-    Route::delete('/cart',[CartController::class,'destroyAll'])->name('cart.destroyAll');
-    Route::delete('/cart/{product}',[CartController::class,'destroy'])->name('cart.destroy');
-    Route::view('/notifications','notifications');
-
 });
 
 Route::middleware('guest')->group(function () {
@@ -28,10 +31,7 @@ Route::middleware('guest')->group(function () {
     Route::post('/register',[RegisteredUserController::class,'store'])->name('register.store');
     Route::get('/login',[AuthenticatedSessionController::class,'create'])->name('login');
     Route::post('/login',[AuthenticatedSessionController::class,'store'])->name('login.store');
-    Route::get('/',[HomeController::class,'index'])->name('home');
     Route::view('/about','about');
-
-
 });
 
 
