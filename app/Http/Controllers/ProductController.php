5e3152c77188a07069f0a8ace2adb3e1604c83d7
Menna-Baligh <mennabaligh55520@yahoo.com>
2025-04-21 15:52:47 +0200

    Added contidtion to handle unauthorized accesss

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,16 +3,31 @@
 namespace App\Http\Controllers;
 
 use App\Models\Product;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ProductController extends Controller
 {
     public function show($productId) {
         $product = Product::findOrFail($productId);
+
         $RelatedProducts = Product::where('category_id', $product->category_id)
             ->where('id', '!=', $productId)
             ->take(4)
             ->get();
-        return view('products.show',compact('product','RelatedProducts'));
+            
+        $user = Auth::user();
+        if ($user) {
+            $rate = $user->reviews()->where('product_id', $productId)->first();
+            if ($rate) {
+                $rateValue = $rate->rate;
+            } else {
+                $rateValue = null;
+            }
+        } else {
+            $rateValue = null;
+        }
+        return view('products.show',compact('product','RelatedProducts','rateValue'));
     }
 }
--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -4,16 +4,24 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ProfileController extends Controller
 {
     public function show(User $user) {
+        
+        if (Auth::user()->id!== $user->id) {
+            abort(403,'not authorized');
+        }
         return view('profile.show' ,[
             'user'=>$user
         ]);
     }
 
     public function edit(User $user) {
+        if (Auth::user()->id!== $user->id) {
+            abort(403,'not authorized');
+        }
         return view('profile.edit',[
             'user'=>$user
         ]);
