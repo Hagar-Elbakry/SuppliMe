48f33fd0dcc22b4a2735e251a726f59ec4f06914
Menna-Baligh <mennabaligh55520@yahoo.com>
2025-05-01 18:29:13 +0300

    updating shopping controller to handel selecting speceific category

--- app/Http/Controllers/ShoppingController.php
+++ app/Http/Controllers/ShoppingController.php
@@ -2,18 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ShoppingController extends Controller
 {
-    public function index(){
+    public function index(Request $request){
         // random products with pagination
         $products = Product::inRandomOrder()->paginate(6);
-        return view('shopping.index',compact('products'));
+        $category_id = $request->query('category');
+        if($category_id){
+            $category = Category::findOrFail($category_id);
+            $products = Product::where('category_id',$category_id)->inRandomOrder()->paginate(6);
+        }
+        $categories = Category::all();
+        return view('shopping.index',compact('products' , 'categories'));
     }
 
-    public function show($id){
-        return view('shopping.show');
-    }
+    
 }
