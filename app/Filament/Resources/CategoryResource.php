002e33840ea03b20438b14e5e6c260d735b901a5
Menna-Baligh <mennabaligh55520@yahoo.com>
2025-09-04 19:10:37 +0300

    Add Category resource with CRUD functionality

---
+++ app/Filament/Resources/CategoryResource.php
@@ -0,0 +1,149 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use Filament\Forms;
+use Filament\Tables;
+use App\Models\Category;
+use Filament\Forms\Form;
+use Faker\Provider\Image;
+use Filament\Tables\Table;
+use Filament\Resources\Resource;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Section;
+use Filament\Tables\Actions\EditAction;
+use Filament\Tables\Actions\ViewAction;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Forms\Components\TextInput;
+use Filament\Tables\Actions\ActionGroup;
+use Filament\Tables\Columns\BadgeColumn;
+use Filament\Tables\Columns\ImageColumn;
+use Filament\Forms\Components\FileUpload;
+use Filament\Tables\Actions\DeleteAction;
+use Filament\Tables\Filters\SelectFilter;
+use Illuminate\Database\Eloquent\Builder;
+use Filament\Forms\Components\MarkdownEditor;
+use App\Filament\Resources\CategoryResource\Pages;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Resources\CategoryResource\RelationManagers;
+
+class CategoryResource extends Resource
+{
+    protected static ?string $model = Category::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Section::make('Basic Information')
+                ->schema([
+                    TextInput::make('name')
+                        ->label('Category Name')
+                        ->required()
+                        ->maxLength(255),
+
+                    MarkdownEditor::make('description')
+                        ->label('Description'),
+                ])
+                ->columns(1),
+
+            Section::make('Visuals')
+                ->schema([
+                    FileUpload::make('image')
+                        ->label('Category Image')
+                        ->image()
+                        ->disk('public')
+                        ->directory('categories')
+                        ->required()
+                        ->imageEditor(),
+
+                    Select::make('color')
+                        ->label('Color')
+                        ->options([
+                            'fre-fru' => 'fre-fru',
+                            'fre-veg' => 'fre-veg',
+                        ])
+                        ->default(null)
+                        ->nullable(),
+                ])
+                ->columns(1),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                ImageColumn::make('image')
+                    ->label('Image')
+                    ->disk('public')
+                    ->getStateUsing(fn ($record) => asset('storage/' . $record->image))
+                    ->rounded(),
+
+                TextColumn::make('name')
+                    ->label('Name')
+                    ->sortable()
+                    ->searchable(),
+
+                TextColumn::make('description')
+                    ->label('Description')
+                    ->limit(20)
+                    ->sortable()
+                    ->searchable()
+                    ->toggleable(),
+
+
+                BadgeColumn::make('color')
+                    ->label('Color')
+                    ->colors([
+                        'warning' => 'fre-fru',
+                        'info' => 'fre-veg',
+                    ])
+                    ->sortable()
+                    ->toggleable(),
+
+                TextColumn::make('created_at')
+                    ->label('Created At')
+                    ->date()
+                    ->sortable(),
+            ])
+            ->filters([
+                SelectFilter::make('color')
+                    ->options([
+                        'fre-fru' => 'fre-fru',
+                        'fre-veg' => 'fre-veg',
+                    ])
+                    ->label('Color')
+            ])
+            ->actions([
+                ActionGroup::make([
+                    ViewAction::make(),
+                    EditAction::make(),
+                    DeleteAction::make(),
+                ])
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListCategories::route('/'),
+            'create' => Pages\CreateCategory::route('/create'),
+            'edit' => Pages\EditCategory::route('/{record}/edit'),
+        ];
+    }
+}
--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,18 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory ;
+    protected $guarded = [];
     public function products(){
         return $this->hasMany(Product::class);
     }
     public function discounts(){
         return $this->hasMany(Discount::class);
     }
-    
+
     public function activeDiscount(){
         return $this->discounts()->where('discount_type','category')
         ->where('start_date','<=',now())
         ->where('end_date','>=',now())
         ->first();
-        
+
     }
 }
