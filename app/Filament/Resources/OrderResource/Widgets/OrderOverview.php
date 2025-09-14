25b3b3767103438e5338bc263738979a969843f9
Hagar Elbakry <hagarelbakry12@gmail.com>
2025-09-14 20:49:30 +0300

    update OrderOverview widget to reflict shipping status and avarage price

--- app/Filament/Resources/OrderResource/Widgets/OrderOverview.php
+++ app/Filament/Resources/OrderResource/Widgets/OrderOverview.php
@@ -2,6 +2,7 @@
 
 namespace App\Filament\Resources\OrderResource\Widgets;
 use App\Models\Order;
+use App\Models\Shipping;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 
@@ -11,9 +12,9 @@ class OrderOverview extends BaseWidget
     {
         return [
             Stat::make('All Orders',  Order::all()->count()),
-            Stat::make('Pending Orders',  Order::where('status', 'pending')->count()),
             Stat::make('Processing Orders',  Order::where('status', 'processing')->count()),
-            Stat::make('Completed Orders',  Order::where('status', 'completed')->count()),
+            Stat::make('Shipped Order',  Shipping::where('status', 'completed')->count()),
+            Stat::make('Average Price',  number_format((Order::avg('total_price')) ?? 0 , 2) . ' EGP')
         ];
     }
 }
