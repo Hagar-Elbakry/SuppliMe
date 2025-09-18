bb85c6cf92f847e47c85fc3658ac2fd71cbc0fc2
Menna-Baligh <mennabaligh55520@yahoo.com>
2025-09-18 17:04:01 +0300

    simplify FavouriteController and reduce code duplication

--- app/Http/Controllers/FavouriteController.php
+++ app/Http/Controllers/FavouriteController.php
@@ -8,46 +8,55 @@ use Illuminate\Support\Facades\Auth;
 
 class FavouriteController extends Controller
 {
+    private function userProducts(){
+        return Auth::user()->products()->get();
+    }
+
     public function index(){
-        $favouriteProducts = Auth::user()->products()->get();
+        $favouriteProducts = $this->userProducts();
         return view('favourite.index',compact('favouriteProducts'));
     }
+
     public function store(Request $request){
         $request->validate([
             'product_id' => 'required|exists:products,id',
         ]);
-        if (Auth::user()->products->contains($request->product_id)) {
-            Auth::user()->products()->detach($request->product_id);
-        } else {
-            Auth::user()->products()->attach($request->product_id);
-        }
+
+        $userProducts = $this->userProducts();
+
+        $userProducts->contains($request->product_id)
+            ? Auth::user()->products()->detach($request->product_id)
+            : Auth::user()->products()->attach($request->product_id);
+
         return redirect()->back();
     }
+
     public function destroy($productId){
-        $favourite = Auth::user()->products()->where('product_id', $productId)->first();
-        if ($favourite) {
-            $favourite->pivot->delete();
-        }
-        return redirect()->back();
+        Auth::user()->products()->detach($productId);
+        return back();
     }
+    
     public function destroyAll(){
         Auth::user()->products()->detach();
         return redirect()->back();
     }
 
     public function addAllToCart(){
-        $favouriteProducts = Auth::user()->products()->get();
+        $favouriteProducts = $this->userProducts();
+        $cart = Cart::firstOrCreate(['user_id' => Auth::id()]);
+
         foreach ($favouriteProducts as $product) {
-            $cart = Cart::firstOrCreate(['user_id' => Auth::id()]);
             $cartProduct = $cart->products()->where('product_id', $product->id)->first();
-            if ($cartProduct) {
-                $cart->products()->updateExistingPivot($product->id ,[
-                    'quantity' => $cartProduct->pivot->quantity + 1
-                ]);
-            } else {
-                $cart->products()->attach($product->id, ['quantity' => 1]);
-            }
+
+            $cart->products()->syncWithoutDetaching([
+                $product->id => [
+                    'quantity' => $cartProduct
+                        ? $cartProduct->pivot->quantity + 1
+                        : 1
+                ]
+            ]);
         }
+
         return redirect()->route('cart.index');
     }
 }
