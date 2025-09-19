db67a38d1f198b69932dd3c65b69b646b95dafa1
Hagar Elbakry <hagarelbakry12@gmail.com>
2025-09-19 03:31:22 +0300

    refactor:clean up CartController by moving long logic to seperated methods and applying model binding

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -7,25 +7,32 @@ use Illuminate\Support\Facades\Auth;
 
 class ProductController extends Controller
 {
-    public function show($productId) {
-        $product = Product::findOrFail($productId);
+    public function show(Product $product)
+    {
+        $product = Product::findOrFail($product->id);
 
         $RelatedProducts = Product::where('category_id', $product->category_id)
-            ->where('id', '!=', $productId)
+            ->where('id', '!=', $product->id)
             ->take(4)
             ->get();
 
+        $rateValue = $this->getRate($product);
+        return view('products.show', compact('product', 'RelatedProducts', 'rateValue'));
+    }
+
+    /**
+     * @param $product
+     * @return null
+     */
+    public function getRate($product): null
+    {
         $user = Auth::user();
         if ($user) {
-            $rate = $user->reviews()->where('product_id', $productId)->first();
-            if ($rate) {
-                $rateValue = $rate->rate;
-            } else {
-                $rateValue = null;
-            }
+            $rate = $user->reviews()->where('product_id', $product->id)->first();
+            $rateValue = $rate ? $rate->rate : null;
         } else {
             $rateValue = null;
         }
-        return view('products.show',compact('product','RelatedProducts','rateValue'));
+        return $rateValue;
     }
 }
